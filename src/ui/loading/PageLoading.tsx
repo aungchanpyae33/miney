6fbad83fa823459066a2f7ebea8d179306474928
aungchanpyae33 <khantlu24@gmail.com>
2026-02-26 16:06:16 +0630

    fix: loading in layout
    in my current phone , ios 15 version , when fetching some data in layout, if the fallback suspense ui does not contain any string or enough string , safari block the entire loading including child loading suspense ui.To fix it , i create the component called SafariStringPlaceHolderNoVisiable.tsx to render string but no visiable
    
    affected components -
    Pageloading.tsx

--- src/ui/loading/PageLoading.tsx
+++ src/ui/loading/PageLoading.tsx
@@ -1,8 +1,10 @@
 import { LoadingIcon } from "../profile/formChild/icons";
+import SafariStringPlaceHolderNoVisiable from "./SafariStringPlaceHolderNoVisiable";
 
 function PageLoading() {
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <SafariStringPlaceHolderNoVisiable />
       <span className=" animate-spin">
         <LoadingIcon width={30} height={30} />
       </span>
