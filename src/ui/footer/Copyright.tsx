90acc2e46e74e3d573adb4f059a07588857b19e4
aungchanpyae33 <khantlu24@gmail.com>
2026-02-28 01:38:38 +0630

    add: include MIT License and update copyright notice in footer component

--- src/ui/footer/Copyright.tsx
+++ src/ui/footer/Copyright.tsx
@@ -1,11 +1,7 @@
 "use client";
 function Copyright() {
   const year = new Date().getFullYear();
-  return (
-    <p className="text-ink-gray-400 text-sm">
-      © {year} AungChanPyae မူပိုင်ခွင့်အားလုံးရှိသည်။
-    </p>
-  );
+  return <p className="text-ink-gray-400 text-sm">© {year} AungChanPyae</p>;
 }
 
 export default Copyright;
