5994123b916fb00757771eb3a9977761bcfe0a68
aungchanpyae33 <khantlu24@gmail.com>
2026-03-04 03:10:05 +0630

    fix: fix the global-not-found-page is stalled
    in global-not-found , dynamic api can't be used as they are not render in normal request context  access, so accessing dynamic api like cookie make the compoment stalled. To fix that i use not-found.tsx page as it is the right choice for this edge case.
    
    the affected components -
    global-not-found.tsx(deleted)
    page.tsx ([...not-found])
    BackToHomegPage.tsx
    NotFoundText.tsx
    NotFoundWrapper.tsx

--- src/ui/general/globalNotFound/NotFoundText.tsx
+++ src/ui/general/globalNotFound/NotFoundText.tsx
@@ -1,7 +1,9 @@
-import { getTranslations } from "next-intl/server";
-
-async function NotFoundText() {
-  const w = await getTranslations("WarningMsg");
+import { _Translator } from "next-intl";
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  w: _Translator<Record<string, any>, "WarningMsg">;
+};
+async function NotFoundText({ w }: { w: Props["w"] }) {
   return <div className=" text-center max-w-80">{w("globalNotFound")}</div>;
 }
 
