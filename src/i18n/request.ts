b963838ad8a491ab1df317efe5d3a89c4eeaf96b
aungchanpyae33 <khantlu24@gmail.com>
2026-03-04 03:18:07 +0630

    feat: add cache the request of message to improve performance

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,20 @@
 import { getRequestConfig } from "next-intl/server";
+import { cacheLife } from "next/cache";
 import { cookies } from "next/headers";
+
+async function getCachedMessages(locale: string) {
+  "use cache";
+  cacheLife("days");
+  return (await import(`../../messages/${locale}.json`)).default;
+}
 
 export default getRequestConfig(async () => {
   const store = await cookies();
   const locale = store.get("locale")?.value || "en";
+
+  const messages = await getCachedMessages(locale);
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
   };
 });
