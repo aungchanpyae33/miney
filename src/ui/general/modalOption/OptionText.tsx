31175fa31f9b35a8f874f28a463261fc33c8df50
aungchanpyae33 <khantlu24@gmail.com>
2026-03-19 15:47:04 +0630

    ui: set different width based on float and mobile case

--- src/ui/general/modalOption/OptionText.tsx
+++ src/ui/general/modalOption/OptionText.tsx
@@ -1,5 +1,5 @@
 function OptionText({ children }: { children: React.ReactNode }) {
-  return <div className=" max-w-[180px] truncate">{children}</div>;
+  return <div className=" flex-1 text-start truncate">{children}</div>;
 }
 
 export default OptionText;
