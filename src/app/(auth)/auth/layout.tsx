40550fbde55b042e1dc79f29edb37ea8d0661832
aungchanpyae33 <khantlu24@gmail.com>
2026-04-04 01:56:20 +0630

    refactor: move deviceCheckFetcher and nextIntl fetcher into app/layout
    As deviceCheckFetcher and nextIntl fetcher are including in (auth) and (root) route group

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,23 +1,14 @@
-import { ReactNode, Suspense } from "react";
-import PageLoading from "@/ui/loading/PageLoading";
-import { NextIntlClientProvider } from "next-intl";
+import { ReactNode } from "react";
 import GeneralSettingOption from "@/ui/navbar/GeneralSettingOption";
-import DeviceCheckFetcher from "@/ui/DeviceCheck/DeviceCheckFetcher";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <Suspense fallback={<PageLoading />}>
-      <DeviceCheckFetcher>
-        <NextIntlClientProvider>
-          <div className=" root container lg:max-w-[1200px]  mx-auto">
-            <div className=" absolute right-4 top-4">
-              <GeneralSettingOption />
-            </div>
+    <div className=" root container lg:max-w-[1200px]  mx-auto">
+      <div className=" absolute right-4 top-4">
+        <GeneralSettingOption />
+      </div>
 
-            {children}
-          </div>
-        </NextIntlClientProvider>
-      </DeviceCheckFetcher>
-    </Suspense>
+      {children}
+    </div>
   );
 }
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,12 +2,10 @@ import Logo from "@/ui/navbar/Logo";
 import NavTopBar from "@/ui/navbar/NavTopBar";
 import NavLink from "@/ui/navbar/NavLink";
 import QueryClientPrv from "@/utils/QueryClient";
-import { NextIntlClientProvider } from "next-intl";
 import { Suspense } from "react";
 import PageLoading from "@/ui/loading/PageLoading";
 import Footer from "@/ui/footer/Footer";
 import ModalBox from "@/ui/general/DialogParent/ModalBox";
-import DeviceCheckFetcher from "@/ui/DeviceCheck/DeviceCheckFetcher";
 import UserInfoFetcher from "@/ui/UserInfoFetch/UserInfoFetcher";
 
 export default function RootLayout({
@@ -19,19 +17,15 @@ export default function RootLayout({
     <div className="root mx-auto max-w-[600px] w-full py-2">
       <QueryClientPrv>
         <Suspense fallback={<PageLoading />}>
-          <DeviceCheckFetcher>
-            <UserInfoFetcher>
-              <NextIntlClientProvider>
-                <NavTopBar>
-                  <Logo />
-                  <NavLink />
-                </NavTopBar>
-                <main className=" p-2">{children}</main>
-                <ModalBox />
-                <Footer />
-              </NextIntlClientProvider>
-            </UserInfoFetcher>
-          </DeviceCheckFetcher>
+          <UserInfoFetcher>
+            <NavTopBar>
+              <Logo />
+              <NavLink />
+            </NavTopBar>
+            <main className=" p-2">{children}</main>
+            <ModalBox />
+            <Footer />
+          </UserInfoFetcher>
         </Suspense>
       </QueryClientPrv>
     </div>
